8980dfad1894cd48f055a1c37d9404cb276e0ef2
lgcanto <lucascanto@poli.ufrj.br>
2018-07-23 23:36:20 -0300

    Adding TotalBsmtSF and GrLiveArea normalization; Final prediction.

--- script_2.py
+++ script_2.py
@@ -8,10 +8,7 @@ from scipy.stats import norm#, skew
 from sklearn.linear_model import Lasso
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import RobustScaler
-#from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor
-#from sklearn.kernel_ridge import KernelRidge
-#from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
-from sklearn.model_selection import KFold, cross_val_score#, train_test_split
+from sklearn.model_selection import KFold, cross_val_score
 from sklearn.metrics import mean_squared_error
 import lightgbm as lgb
 def ignore_warn(*args, **kwargs):
@@ -31,6 +28,8 @@ test_ID = test['Id']
 train.drop("Id", axis = 1, inplace = True)
 test.drop("Id", axis = 1, inplace = True)
 
+###################################################################################################
+
 #Verificando a matriz de correlação com as 10 variáveis mais correlatas à SalePrice
 corrmat = train.corr()
 numVariaveis = 10
@@ -45,6 +44,8 @@ sns.set()
 cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars', 'TotalBsmtSF', 'FullBath', 'YearBuilt']
 sns.pairplot(train[cols], size = 2.5)
 plt.show();
+
+###################################################################################################
 
 #Remoção de Outliers
 #Verificamos que OverallQual é a variável com maior correlação. Mas como esta é 
@@ -86,8 +87,9 @@ plt.ylabel('SalePrice', fontsize=13)
 plt.xlabel('TotalBsmtSF', fontsize=13)
 plt.show()
 
+###################################################################################################
 
-#Normalização de SalePrice
+#Normalização de SalePrice, feito apenas no dataset de treino
 #Histograma e probabilidade:
 sns.distplot(train['SalePrice'], fit=norm);
 fig = plt.figure()
@@ -101,6 +103,8 @@ sns.distplot(train['SalePrice'], fit=norm);
 fig = plt.figure()
 res = stats.probplot(train['SalePrice'], plot=plt)
 
+###################################################################################################
+
 #Manipulação de features:
 #Como a manipulação de varíaveis categóricas e dados nulos deve ser feito
 #tanto no dataset de treino quanto no de test, os datasets serão concatenados
@@ -109,8 +113,11 @@ ntrain = train.shape[0]
 ntest = test.shape[0]
 y_train = train.SalePrice.values
 all_data = pd.concat((train, test)).reset_index(drop=True)
+
 #Removendo coluna SalePrice que não está presente no dataset de teste:
 all_data.drop(['SalePrice'], axis=1, inplace=True)
+
+###################################################################################################
 
 #Dados nulos:
 all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
@@ -121,8 +128,46 @@ missing_data.head(50)
 #Preenchendo vazios utilizando foward fill:
 all_data = all_data.fillna(method='ffill')
 
+###################################################################################################
+
+#Normalização de GrLivArea, em ambos datasets
+#Histograma e probabilidade:
+sns.distplot(all_data['GrLivArea'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(all_data['GrLivArea'], plot=plt)
+
+#Aplicando transformação logarítma:
+all_data['GrLivArea'] = np.log(all_data['GrLivArea'])
+
+#Replotando:
+sns.distplot(all_data['GrLivArea'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(all_data['GrLivArea'], plot=plt)
+
+#Normalização de TotalBsmtSF, em ambos datasets
+#Histograma e probabilidade:
+sns.distplot(all_data['TotalBsmtSF'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(all_data['TotalBsmtSF'], plot=plt)
+
+all_data['HasBsmt'] = pd.Series(len(all_data['TotalBsmtSF']), index=all_data.index)
+all_data['HasBsmt'] = 0 
+all_data.loc[all_data['TotalBsmtSF']>0,'HasBsmt'] = 1
+
+#Aplicando transformação logarítma:
+all_data.loc[all_data['HasBsmt']==1,'TotalBsmtSF'] = np.log(all_data['TotalBsmtSF'])
+
+#Replotando:
+sns.distplot(all_data[all_data['TotalBsmtSF']>0]['TotalBsmtSF'], fit=norm);
+fig = plt.figure()
+res = stats.probplot(all_data[all_data['TotalBsmtSF']>0]['TotalBsmtSF'], plot=plt)
+
+###################################################################################################
+
 #Lidando com variáveis categóricas transformando-as em "dummies":
 all_data = pd.get_dummies(all_data)
+
+###################################################################################################
 
 #Separando os datasets:
 train = all_data[:ntrain]
@@ -139,6 +184,10 @@ def rmsle_cv(model):
 def rmsle(y, y_pred):
     return np.sqrt(mean_squared_error(y, y_pred))
 
+###################################################################################################
+
+#Definição da técnica utilizada para minimazação do erro: descida de gradiente
+#utilizando LightGBM
 model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
                               learning_rate=0.05, n_estimators=720,
                               max_bin = 55, bagging_fraction = 0.8,
@@ -148,21 +197,24 @@ model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
 score = rmsle_cv(model_lgb)
 print("LGBM score: {:.4f} ({:.4f})\n" .format(score.mean(), score.std()))
 
+#Realizando o treino através do dataset de treino:
 model_lgb.fit(train, y_train)
 lgb_train_pred = model_lgb.predict(train)
 final_pred = np.expm1(model_lgb.predict(test.values))
 print(rmsle(y_train, lgb_train_pred))
 
-lasso = make_pipeline(RobustScaler(), Lasso(alpha =0.0005, random_state=1))
-score = rmsle_cv(lasso)
-print("\nLasso score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
+#Caso o pacote do LightGBM não funcione, usar o Lasso: (comentar a parte
+#do LightGBM e descomentar esta parte abaixo do Lasso)
+#lasso = make_pipeline(RobustScaler(), Lasso(alpha =0.0005, random_state=1))
+#score = rmsle_cv(lasso)
+#print("\nLasso score: {:.4f} ({:.4f})\n".format(score.mean(), score.std()))
 
-#Caso o pacote do LightGBM não funcione, usar o Lasso:
 #lasso.fit(train.values, y_train)
 #lasso_train_pred = lasso.predict(train.values)
 #final_pred = np.expm1(lasso.predict(test.values))
 #print(rmsle(y_train, lasso_train_pred))
 
+#Salvando no .csv:
 sub = pd.DataFrame()
 sub['Id'] = test_ID
 sub['SalePrice'] = final_pred
